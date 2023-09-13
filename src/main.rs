25e849a057d80fcf17010a972ef98820b4a59be2
Monius <m1105930979@gmail.com>
2023-09-14 06:13:28 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,25 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
-use std::fs;
-use std::io::Read;
+// use std::fs;
+// use std::io::Read;
 
 async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     if req.method() == hyper::Method::GET{
-        if req.uri().path() == "/data.zip" {
-            let mut file = fs::File::open("tensorboard_log.zip").unwrap();
-            let mut contents: Vec<u8> = Vec::new();
-            file.read_to_end(&mut contents).unwrap();
-            let response = Response::builder()
-                .header("Content-Type", "application/octet-stream")
-                .body(Body::from(contents))
-                .unwrap();
-            Ok(response)
-        }
-        else {
-            Ok(Response::new("Hello, World".into()))
-        }
-
+        // if req.uri().path() == "/data.zip" {
+        //     let mut file = fs::File::open("tensorboard_log.zip").unwrap();
+        //     let mut contents: Vec<u8> = Vec::new();
+        //     file.read_to_end(&mut contents).unwrap();
+        //     let response = Response::builder()
+        //         .header("Content-Type", "application/octet-stream")
+        //         .body(Body::from(contents))
+        //         .unwrap();
+        //     Ok(response)
+        // }
+        // else {
+        //     Ok(Response::new("Hello, World".into()))
+        // }
+        Ok(Response::new("Hello, World".into()))
     } else {
         let response = Response::builder()
             .status(404)
@@ -32,7 +32,7 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
 
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([0, 0, 0, 0], 9091));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
     let make_svc = make_service_fn(|_conn| async {
         Ok::<_, Infallible>(service_fn(handle_request) )
